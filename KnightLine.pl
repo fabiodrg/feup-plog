f333066240d74713dad13bff9b46179ae1a00c5e
Diogo Mota Pinto <diogomotapinto@gmail.com>
2018-10-15 21:43:17 +0100

    file added

---
+++ KnightLine.pl
@@ -0,0 +1,68 @@
+%%%%%%%% Pecas %%%%%%
+
+tile(e).
+
+tile(b-X):-
+    integer(X).
+
+tile(w-X):-
+    integer(X).
+
+player(b).
+
+player(w).
+
+
+%%% Boards %%%%%
+
+initialBoard([
+        [e,e,e,e],
+        [e,b-20,w-20,e],
+        [e,e,e,e]    
+    ]).
+
+
+finalBoard([
+    [e,e,e,b-4,e,e],
+    [e,e,w-4,b-4,b-4,b-1],
+    [e,e,b-3,w-3,w-3,e],
+    [b-2,w-2,w-2,w-2,b-2,e],
+    [e,e,w-2,w-2,e,e],
+    [e,w-2,e,b-2,e,e]
+    ]).
+
+
+%% print board %%
+
+
+print_board([],P):-
+    write(' -'),
+    translate(P,W),
+    write(W).
+
+
+print_board([H|T],P):-
+    print_line(H),
+    nl,
+    print_board(T,P).
+
+print_line([]).
+print_line([H|T]):-
+    print_cell(H),
+    print_line(T).
+
+print_cell(X-Y):-
+    translate(X,W),
+    write(W),
+    write('-'),
+    write(Y).
+
+print_cell(X):-
+    translate(X,W),
+    write(W).
+
+translate(b,'B').
+translate(w,'W').
+translate(e,'E').
+
+
