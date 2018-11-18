764d2aff50308eb4d2fdf9886dbcb39de569dfab
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-11-18 21:56:13 +0000

    Revert "Fixed some singleton warnings"
    This reverts commit 635e9fc8e57c1c744a5ad73bbb6bd97f18bd18af.

--- KnightLine.pl
+++ KnightLine.pl
@@ -46,11 +46,15 @@ finalBoard([
 
 %% print board %%
 
+test_insertBoard2:-
+  midBoard(Board),
+  display_withcoords(Board).
+
 display_withcoords(Board):-
   insert_Coords(Board,X1),
   display_game(X1).
 
-print_initial_board:-
+print_initial_board(Y):-
     initialBoard(X),
     insert_Coords(X,Z),
     display_game(Z).
@@ -122,7 +126,7 @@ value(Board,Player,Value):-
   write(Results),
   check_all_pieces(Results,Board,Player,Value).
 
-check_all_pieces([], _, _, _).
+check_all_pieces([],Board,Value,Player).
 check_all_pieces([H|T], Board,Value,Player):-
   check_down(H,Board,0,Player,NewValue1),
   write(NewValue1),
@@ -313,7 +317,7 @@ valid_moves(Board,Player,ListOfMoves):-
 /*
 *Remove the positions in the board already occupied by another piece.
 */
-remove_results(_,[],_,_).
+remove_results(Board,[],List,List).
 remove_results(Board,[H|T],ResultList,List):-
   take_piece(H,Board,Piece),
   Piece \= e ->
