dfebfe69b405bc4f9cdee9a22bbc0efab74ae345
Fabio Gaspar <fabiodanielreis@hotmail.com>
2018-12-20 23:54:59 +0000

    Further work

--- House/solver.pl
+++ House/solver.pl
@@ -1,8 +1,14 @@
 :-use_module(library(clpfd)).
-:-use_module(library(lists))
+:-use_module(library(lists)).
 
 /**/
 example(
+	[
+		[0,0], [1,0], 
+		[0,1], [2,1]
+	]).
+
+example1(
 	[
 		[0,0], [2,0], [3,0],
 		[2,1], [3,1],
@@ -10,7 +16,10 @@ example(
 		[0,3], [3,3] 
 	]).
 
-
+example2(
+	[
+		[0,0], [0,3], [0,4], [0,5], [0,1], [0,8]
+	]).
 /*
  * P1 - List [X1, Y1]
  * P2 - List [X2, Y2]
@@ -26,33 +35,39 @@ distance(P1, P2, D):-
 
 solver(ListHouses, PuzzleSize):-
 	% a list with the two possible distances (distinct) %
-	domain([D1,D2], 1, sup),
+	MaxDistance is PuzzleSize*PuzzleSize,
+	domain([D1,D2], 1, MaxDistance),
 	all_distinct([D1,D2]),
 	% create an auxiliar list with indexes for the list of houses %
 	% the elements in list of houses are lists with a pair of values X and Y %
 	% the predicate element only works with indexes, thus we need list of houses %
-	length(Indexes, PuzzleSize), % fix size, get length list houses %
-	domain(Indexes, 1, PuzzleSize),
+	length(ListHouses, NumHouses),
+	length(Indexes, NumHouses),
+	domain(Indexes, 1, NumHouses),
 	all_distinct(Indexes),
+	labeling([],Indexes),
 	% find the distances %
-	findDistances(ListHouses, D1, D2),
-	write(D1),
+	findDistances(Indexes, ListHouses, NumHouses, D1, D2),
+	labeling([],[D1,D2]),
+	write(D1), write('-'),
 	write(D2).
-			
 
-findDistances([], _, _, _).
+findDistances([], _, _, _, _).
 % The plog  %
-findDistances(Indexes, ListHouses, D1, D2):-
+findDistances(Indexes, ListHouses, NumHouses, D1, D2):-
+	domain([IndexHouse1, IndexHouse2], 1, NumHouses),
 	% pick one house %
-	element(H1Index, Indexes, H1Index),
-	nth1(H1Index, ListHouses, House1),
+	element(IndexHouse1, Indexes, IndexHouse1),
+	nth1(IndexHouse1, ListHouses, P1),
 	% pick second house %
-	element(H2Index, Indexes, H2Index),
-	nth1(H2Index, ListHouses, House2),
+	element(IndexHouse2, Indexes, IndexHouse2),
+	nth1(IndexHouse2, ListHouses, P2),
+	% ensure both index are different %
+	IndexHouse1 #\= IndexHouse2,
 	% compute distance %
-	distance(House1, House2, D),
+	distance(P1, P2, D),
 	D #= D1 #\ D #= D2, % attempt to link D to D1 or D2
 	% remove the indexes from list %
-	delete(Indexes, H1Index, Aux1),
-	delete(Aux1, H2Index, Aux2),
-	findDistances(Aux2, ListHouses, D1,  D2).
+	delete(Indexes, IndexHouse1, Aux1),
+	delete(Aux1, IndexHouse2, Aux2),
+	findDistances(Aux2, ListHouses, NumHouses, D1,  D2).
